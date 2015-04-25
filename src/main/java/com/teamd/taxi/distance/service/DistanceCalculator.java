9c3491cc248f8db361448b828e12c90e9f112d33
xtfkpi <xtfkpi@xtfkpi-Lenovo-E10-30>
2015-04-25 18:25:58 +0300

    DistanceCalculator added

---
+++ src/main/java/com/teamd/taxi/distance/service/DistanceCalculator.java
@@ -0,0 +1,79 @@
+package com.teamd.taxi.distance.service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class DistanceCalculator {
+
+	public static void main(String args[]) {
+		System.out.println("distance="+distance("Киев, Крещатик 1", "Киев, проспект победы 22"));
+		System.out.println("distance="+distance("50.45,31", "50,30"));
+	}
+
+	public static int distance(String a, String b) {
+		String url = null;
+		String json = null;
+
+		try {
+			url = "http://maps.googleapis.com/maps/api/directions/json?origin="
+					+ URLEncoder.encode(a, "UTF-8") + "&destination="
+					+ URLEncoder.encode(b, "UTF-8") + "&region=ua&sensor=false";
+		} catch (UnsupportedEncodingException e2) {
+			System.out.println("something strange with encoding");
+			e2.printStackTrace();
+		}
+
+		try {
+			json = getJson(url);
+		} catch (Exception e1) {
+			System.out.println("something strange with your json");
+			e1.printStackTrace();
+		}
+
+		JSONParser parser = new JSONParser();
+
+		Object obj = null;
+		try {
+			obj = parser.parse(json);
+		} catch (ParseException e) {
+			System.out.println("error with parsing");
+			e.printStackTrace();
+		}
+		JSONObject jsonObj = (JSONObject) obj;
+		System.out.println(jsonObj);
+
+		JSONArray ja = (JSONArray) jsonObj.get("routes");
+		jsonObj = (JSONObject) ja.get(0);
+		ja = (JSONArray) jsonObj.get("legs");
+		jsonObj = (JSONObject) ja.get(0);
+		jsonObj = (JSONObject) jsonObj.get("distance");
+		return Integer.parseInt(jsonObj.get("value").toString());
+	}
+
+	public static String getJson(String pageAddress) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		URL pageURL = new URL(pageAddress);
+		URLConnection uc = pageURL.openConnection();
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				uc.getInputStream()));
+		try {
+			String inputLine;
+			while ((inputLine = br.readLine()) != null) {
+				sb.append(inputLine);
+			}
+		} finally {
+			br.close();
+		}
+		return sb.toString();
+	}
+
+}
